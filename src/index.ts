59c88dfd210a93485c7c7fed15de29edd2ea5415
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-11-22 12:39:56 +0100

    added app init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import { port } from './settings';
+import dotenv from 'dotenv';
+import app from './app';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+dotenv.config();
+const port = process.env.PORT;
 
-app.get('/api/dummy-endpoint', (req, res) =>
-  res.send({
-    message: 'Hello from the backend',
-  })
-);
-
-app.listen(port, () => console.log(`listening on ${port}`));
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`listening on ${port || 3000}`));
