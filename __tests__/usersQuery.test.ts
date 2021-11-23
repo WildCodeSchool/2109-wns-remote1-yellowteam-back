8337f0035346dde0ef3d297bbc1c56099664f7c9
JIdayyy <julien_abbadie@hotmail.fr>
2021-11-23 12:18:33 +0100

    validate: user and users query tests and database test logic

--- __tests__/usersQuery.test.ts
+++ __tests__/usersQuery.test.ts
@@ -1,21 +1,49 @@
+/* eslint-disable no-console */
+import faker from 'faker';
+import { Role } from '.prisma/client';
+import prismaClient from '../prisma/prismaClient';
 import server from '../src/server';
 
 const GET_USERS = `
 query getUsers{
     users {
-        id
-        name
-        email
-        password
+      id
+      name
+      email
+      password
+      isDisabled
+      role
     }
 }
 `;
 
 test('server start and return an array of users', async () => {
   server.start();
+
+  const fakeUsers = new Array(20).fill('').map(() => ({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    isDisabled: false,
+    role: ['USER' as Role],
+  }));
+
+  await Promise.all(
+    fakeUsers.map((user) =>
+      prismaClient.user.create({
+        data: user,
+      })
+    )
+  );
+
+  const allPrismaUsers = await prismaClient.user.findMany();
+
   const AllUsersResult = await server.executeOperation({
     query: GET_USERS,
   });
 
   expect(AllUsersResult.errors).toBeUndefined();
+  expect(AllUsersResult.data!.users).toEqual(allPrismaUsers);
+
+  console.log('test passed successfully 👍');
 });
