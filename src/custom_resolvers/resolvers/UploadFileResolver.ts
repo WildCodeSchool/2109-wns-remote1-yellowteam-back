7c16e5f0a338852e4fc3fbb6630e3c0681aca2cc
JIdayyy <julien_abbadie@hotmail.fr>
2021-11-25 23:49:51 +0100

    todo:upload resolver almost done

--- src/custom_resolvers/resolvers/UploadFileResolver.ts
+++ src/custom_resolvers/resolvers/UploadFileResolver.ts
@@ -3,6 +3,8 @@ import { GraphQLUpload } from 'graphql-upload';
 import { Arg, Ctx } from 'type-graphql';
 import { createWriteStream } from 'fs';
 import { Stream } from 'stream';
+import { getPrismaFromContext } from 'src/type_graphql/helpers';
+import { PrismaClient } from '.prisma/client';
 
 interface Upload {
   filename: string;
@@ -17,22 +19,43 @@ export class UploadFile {
     nullable: false,
   })
   async uploadFile(
-    @Ctx() ctx: any,
+    @Ctx() ctx: { prisma: PrismaClient },
     @Arg('file', () => GraphQLUpload) { createReadStream, filename }: Upload
   ): Promise<Boolean> {
-    console.log('ok your in');
-    console.log(filename);
-    return new Promise(async (resolve, reject) =>
-      createReadStream()
-        .pipe(createWriteStream(__dirname + `/../../../uploads/${filename}`))
-        .on('finish', (e) => {
-          console.log(e);
-          resolve(true);
-        })
-        .on('error', (e) => {
-          console.log(e);
-          reject(false);
-        })
+    console.log(
+      await new Promise(async (resolve, reject) =>
+        createReadStream()
+          .pipe(createWriteStream(__dirname + `/../../../uploads/${filename}`))
+          .on('finish', async (e) => {
+            console.log(e);
+            const file = await ctx.prisma.file.create({
+              data: {
+                name: 'test',
+                path: 'test',
+                size: 2,
+                type: 'test',
+                is_disabled: false,
+                user: {
+                  connect: {
+                    id: '0d5215f1-f3f2-40c9-8875-35e7d3c01341',
+                  },
+                },
+                project: {
+                  connect: {
+                    id: '2e4912bb-7491-4251-ab70-108495cc6039',
+                  },
+                },
+              },
+            });
+            console.log(file);
+            resolve(true);
+          })
+          .on('error', (e) => {
+            console.log(e);
+            reject(false);
+          })
+      )
     );
+    return true;
   }
 }
