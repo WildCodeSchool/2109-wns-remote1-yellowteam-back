142f7d55a048b02ac5b2311d4da72bbc07086166
JIdayyy <julien_abbadie@hotmail.fr>
2021-11-26 11:36:24 +0100

    prisma schema and seeds logic

--- prisma/migrations/20211125171725_task_id_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "File" DROP CONSTRAINT "File_taskId_fkey";
-
--- AlterTable
-ALTER TABLE "File" ALTER COLUMN "taskId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "File" ADD CONSTRAINT "File_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE SET NULL ON UPDATE CASCADE;
