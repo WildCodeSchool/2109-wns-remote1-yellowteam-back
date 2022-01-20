88fbf111b1f8c4de873d45946c80954572ad2fc0
JIdayyy <julien_abbadie@hotmail.fr>
2022-01-20 16:59:02 +0100

    feat: 🎸 cors in express dummy handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 
+import cors from 'cors';
+
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.get('/', (req, res) => res.status(200).send('Hello World'));
 
