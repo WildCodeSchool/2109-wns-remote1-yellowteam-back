5143d4d38f03460ff14331ac16f72ed7f2b38c30
lilykr <lisa-lou.k@hotmail.fr>
2022-03-28 17:35:34 +0200

    logout

--- src/generated/graphql/resolvers/inputs/ProjectCreateManyInput.ts
+++ src/generated/graphql/resolvers/inputs/ProjectCreateManyInput.ts
@@ -44,9 +44,9 @@ export class ProjectCreateManyInput {
   status_project!: "IN_PROGRESS" | "NOT_STARTED" | "FIHISHED";
 
   @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
+    nullable: true
   })
-  total_time_spent!: number;
+  total_time_spent?: number | undefined;
 
   @TypeGraphQL.Field(_type => Date, {
     nullable: false
