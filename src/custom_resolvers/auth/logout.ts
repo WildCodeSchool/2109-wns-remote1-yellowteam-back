88898d91b6c2017f57fda37f0e23bc242daee7dc
lilykr <lisa-lou.k@hotmail.fr>
2022-03-28 17:39:04 +0200

    lint

--- src/custom_resolvers/auth/logout.ts
+++ src/custom_resolvers/auth/logout.ts
@@ -4,15 +4,14 @@ import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 import Cookies from 'cookies';
 
-
 @Resolver()
 export class LogoutResolver {
   @Mutation(() => String)
   async logout(
-    @Ctx() ctx: { prisma: PrismaClient; req: Request; res: Response },
+    @Ctx() ctx: { prisma: PrismaClient; req: Request; res: Response }
   ): Promise<String> {
-     const cookies = new Cookies(ctx.req, ctx.res)
-     cookies.set('token', '')
-      return 'User successfully logged out'
-}
+    const cookies = new Cookies(ctx.req, ctx.res);
+    cookies.set('token', '');
+    return 'User successfully logged out';
+  }
 }
