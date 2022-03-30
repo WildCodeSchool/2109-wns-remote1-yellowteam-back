58be08d94ed6cc8549d3f5e85e92b6b7687fb11f
JIdayyy <julien_abbadie@hotmail.fr>
2022-03-30 16:22:49 +0200

    fix: 🐛 whitelist env variblae

--- src/constants/cors.whitelist.ts
+++ src/constants/cors.whitelist.ts
@@ -1,10 +1,3 @@
-const whitelist = [
-  'http://localhost:19000',
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://83.193.203.215:3000',
-  'http://92.89.86.202:3000',
-  'http://192.168.1.26:19000',
-];
+const whitelist = [process.env.CLIENT_WHITELIST];
 
 export default whitelist;
